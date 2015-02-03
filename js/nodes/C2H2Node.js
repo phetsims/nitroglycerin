9a2f5780be9c2dfc7165a8000839e8d1c05b89e1
Chris Malley <cmalley@pixelzoom.com>
2015-02-03 16:36:53 -0700

    #4 reimplement C2H2 as a subtype of HorizontalMoleculeNode

--- js/nodes/C2H2Node.js
+++ js/nodes/C2H2Node.js
@@ -9,41 +9,19 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var AtomNode = require( 'NITROGLYCERIN/nodes/AtomNode' );
   var Element = require( 'NITROGLYCERIN/Element' );
+  var HorizontalMoleculeNode = require( 'NITROGLYCERIN/nodes/HorizontalMoleculeNode' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
-  var Vector2 = require( 'DOT/Vector2' );
 
   /**
    * @param {Object} [options]
    * @constructor
    */
   function C2H2Node( options ) {
-
-    options = _.extend( { atomOptions: {} }, options );
-
-    // atoms
-    var bigLeftNode = new AtomNode( Element.C, options.atomOptions );
-    var bigRightNode = new AtomNode( Element.C, _.extend( {
-      x: bigLeftNode.right + ( 0.25 * bigLeftNode.width ),
-      y: bigLeftNode.y
-    }, options.atomOptions ) );
-    var smallLeftNode = new AtomNode( Element.H, _.extend( {
-      x: bigLeftNode.left,
-      y: bigLeftNode.y
-    }, options.atomOptions ) );
-    var smallRightNode = new AtomNode( Element.H, _.extend( {
-      x: bigRightNode.right,
-      y: bigLeftNode.y
-    }, options.atomOptions ) );
-
-    options.children = [ new Node( {
-      children: [ smallLeftNode, bigLeftNode, bigRightNode, smallRightNode ],
-      center: Vector2.ZERO // origin at geometric center
-    } ) ];
-    Node.call( this, options );
+    options = _.extend( {}, options );
+    options.overlapPercent = 0.35;
+    HorizontalMoleculeNode.call( this, [ Element.H, Element.C, Element.C, Element.H ], options );
   }
 
-  return inherit( Node, C2H2Node );
+  return inherit( HorizontalMoleculeNode, C2H2Node );
 } );
