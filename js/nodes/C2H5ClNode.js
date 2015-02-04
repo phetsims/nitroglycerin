3388a3530e1a73bb68a5503733e3b72d2a368e94
Chris Malley <cmalley@pixelzoom.com>
2015-02-03 17:23:02 -0700

    #4 rework C2H5Cl

--- js/nodes/C2H5ClNode.js
+++ js/nodes/C2H5ClNode.js
@@ -24,58 +24,44 @@ define( function( require ) {
 
     options = _.extend( { atomOptions: {} }, options );
 
-    Node.call( this );
-
-    // atom nodes
+    // atoms
     var leftNode = new AtomNode( Element.C, options.atomOptions );
-    var centerNode = new AtomNode( Element.C, options.atomOptions );
-    var smallTopLeftNode = new AtomNode( Element.H, options.atomOptions );
-    var smallBottomLeftNode = new AtomNode( Element.H, options.atomOptions );
-    var smallLeftNode = new AtomNode( Element.H, options.atomOptions );
-    var smallTopRightNode = new AtomNode( Element.H, options.atomOptions );
-    var smallBottomRightNode = new AtomNode( Element.H, options.atomOptions );
+    var centerNode = new AtomNode( Element.C, _.extend( {
+      x: leftNode.right + ( 0.25 * leftNode.width ),
+      y: leftNode.y
+    }, options.atomOptions ) );
+    var smallTopLeftNode = new AtomNode( Element.H, _.extend( {
+      x: leftNode.x,
+      y: leftNode.top
+    }, options.atomOptions ) );
+    var smallBottomLeftNode = new AtomNode( Element.H, _.extend( {
+      x: smallTopLeftNode.x,
+      y: leftNode.bottom
+    }, options.atomOptions ) );
+    var smallLeftNode = new AtomNode( Element.H, _.extend( {
+      x: leftNode.left,
+      y: leftNode.y
+    }, options.atomOptions ) );
+    var smallTopRightNode = new AtomNode( Element.H, _.extend( {
+      x: centerNode.x,
+      y: centerNode.top
+    }, options.atomOptions ) );
+    var smallBottomRightNode = new AtomNode( Element.H, _.extend( {
+      x: centerNode.x,
+      y: centerNode.bottom
+    }, options.atomOptions ) );
     var rightNode = new AtomNode( Element.Cl, options.atomOptions );
+    rightNode.x = centerNode.right + ( 0.125 * rightNode.width );
+    rightNode.y = centerNode.y;
 
-    // rendering order
-    var parentNode = new Node();
-    this.addChild( parentNode );
-    parentNode.addChild( smallBottomRightNode );
-    parentNode.addChild( smallTopRightNode );
-    parentNode.addChild( centerNode );
-    parentNode.addChild( rightNode );
-    parentNode.addChild( smallLeftNode );
-    parentNode.addChild( leftNode );
-    parentNode.addChild( smallBottomLeftNode );
-    parentNode.addChild( smallTopLeftNode );
-
-    // layout
-    var x = 0;
-    var y = 0;
-    leftNode.setTranslation( x, y );
-    x = leftNode.right + ( 0.25 * centerNode.width );
-    y = leftNode.y;
-    centerNode.setTranslation( x, y );
-    x = leftNode.x;
-    y = leftNode.top;
-    smallTopLeftNode.setTranslation( x, y );
-    x = smallTopLeftNode.x;
-    y = leftNode.bottom;
-    smallBottomLeftNode.setTranslation( x, y );
-    x = leftNode.left;
-    y = leftNode.y;
-    smallLeftNode.setTranslation( x, y );
-    x = centerNode.x;
-    y = centerNode.top;
-    smallTopRightNode.setTranslation( x, y );
-    x = centerNode.x;
-    y = centerNode.bottom;
-    smallBottomRightNode.setTranslation( x, y );
-    x = centerNode.right + ( 0.125 * rightNode.width );
-    y = centerNode.y;
-    rightNode.setTranslation( x, y );
-
-    // move origin to geometric center
-    parentNode.center = Vector2.ZERO;
+    options.children = [ new Node( {
+      children: [
+        smallBottomRightNode, smallTopRightNode, centerNode, rightNode,
+        smallLeftNode, leftNode, smallBottomLeftNode, smallTopLeftNode
+      ],
+      center: Vector2.ZERO // origin at geometric center
+    } ) ];
+    Node.call( this, options );
   }
 
   return inherit( Node, C2H5ClNode );
