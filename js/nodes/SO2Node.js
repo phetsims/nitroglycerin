c85fdf6ba5fa9ccd7942ed4988d8fad83ee48d23
Chris Malley <cmalley@pixelzoom.com>
2015-02-03 17:31:37 -0700

    #4 revise SO2

--- js/nodes/SO2Node.js
+++ js/nodes/SO2Node.js
@@ -25,14 +25,14 @@ define( function( require ) {
 
     // atoms
     var centerNode = new AtomNode( Element.S, options.atomOptions );
-    var leftNode = new AtomNode( Element.O, options.atomOptions );
-    var rightNode = new AtomNode( Element.O, options.atomOptions );
-
-    // layout
-    leftNode.x = centerNode.left;
-    leftNode.y = centerNode.y + ( 0.25 * leftNode.height );
-    rightNode.x = centerNode.right;
-    rightNode.y = centerNode.y + ( 0.25 * rightNode.height );
+    var leftNode = new AtomNode( Element.O, _.extend( {
+      x: centerNode.left,
+      y: centerNode.y + ( 0.2 * centerNode.height )
+    }, options.atomOptions ) );
+    var rightNode = new AtomNode( Element.O, _.extend( {
+      x: centerNode.right,
+      y: centerNode.y + ( 0.2 * centerNode.height )
+    }, options.atomOptions ) );
 
     options.children = [ new Node( {
       children: [ leftNode, centerNode, rightNode ],
