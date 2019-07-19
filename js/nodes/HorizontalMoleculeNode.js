c0f3242fc3de6b53097e6714b5d3d7a950e8bf53
phet-dev <phetadmin@gmail.com>
2019-07-19 03:20:10 -0600

    update copyright dates from daily grunt work

--- js/nodes/HorizontalMoleculeNode.js
+++ js/nodes/HorizontalMoleculeNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2015, University of Colorado Boulder
+// Copyright 2013-2017, University of Colorado Boulder
 
 /**
  * Base class for molecules with N atoms aligned on the horizontal axis, for N > 0.
