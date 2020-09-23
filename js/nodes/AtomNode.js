5de18ce0a6f5d771820ebd21a2d27d6002900c8d
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 18:44:35 -0600

    convert AtomNode to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/nodes/AtomNode.js
+++ js/nodes/AtomNode.js
@@ -7,35 +7,40 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import ShadedSphereNode from '../../../scenery-phet/js/ShadedSphereNode.js';
 import Element from '../Element.js';
 import nitroglycerin from '../nitroglycerin.js';
 
 // constants
-const rateOfChange = 0.75; // >0 and <1, increase this to make small atoms appear smaller
-const maxRadius = Element.P.covalentRadius; // not actually the maximum, but this is a constant from the previous version
-const modelToViewScale = 0.11;
+const RATE_OF_CHANGE = 0.75; // >0 and <1, increase this to make small atoms appear smaller
+const MAX_RADIUS = Element.P.covalentRadius; // not actually the maximum, but this is a constant from the previous version
+const MODEL_TO_VIEW_SCALE = 0.11;
+
+class AtomNode extends ShadedSphereNode {
+
+  /**
+   * @param {Element} element
+   * @param {Object} [options]
+   */
+  constructor( element, options ) {
+
+    options = merge( {
+      mainColor: element.color
+    }, options );
+
+    super( 2 * radiusScalingFunction( element.covalentRadius ), options );
+  }
+}
 
 /*
  * There is a large difference between the radii of the smallest and largest atoms.
  * This function adjusts scaling so that the difference is still noticeable, but not as large.
  */
 function radiusScalingFunction( radius ) {
-  const adjustedRadius = ( maxRadius - rateOfChange * ( maxRadius - radius ) );
-  return modelToViewScale * adjustedRadius;
+  const adjustedRadius = ( MAX_RADIUS - RATE_OF_CHANGE * ( MAX_RADIUS - radius ) );
+  return MODEL_TO_VIEW_SCALE * adjustedRadius;
 }
 
-const AtomNode = function AtomNode( element, options ) {
-  options = merge( {
-    mainColor: element.color // passed to ShadedSphereNode
-  }, options );
-
-  ShadedSphereNode.call( this, 2 * radiusScalingFunction( element.covalentRadius ), options );
-};
-
 nitroglycerin.register( 'AtomNode', AtomNode );
-
-inherit( ShadedSphereNode, AtomNode );
 export default AtomNode;
