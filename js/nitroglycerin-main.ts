1605e1018eacd77cdbf9607be189f3aea4e0cb6a
pixelzoom <cmalley@pixelzoom.com>
2022-07-18 14:12:14 -0600

    add nitroglycerin demo

---
+++ js/nitroglycerin-main.ts
@@ -0,0 +1,42 @@
+// Copyright 2014-2022, University of Colorado Boulder
+
+/**
+ * Main file for the nitroglycerin library demo.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
+import Property from '../../axon/js/Property.js';
+import Screen from '../../joist/js/Screen.js';
+import Sim, { SimOptions } from '../../joist/js/Sim.js';
+import simLauncher from '../../joist/js/simLauncher.js';
+import { Color } from '../../scenery/js/imports.js';
+import Tandem from '../../tandem/js/Tandem.js';
+import MoleculesScreenView from './demo/MoleculesScreenView.js';
+
+function createEmptyModel() {
+  return {};
+}
+
+simLauncher.launch( () => {
+
+  const title = 'nitroglycerin';
+
+  const screens = [
+    new Screen(
+      createEmptyModel,
+      () => new MoleculesScreenView(), {
+        name: 'Molecules',
+        backgroundColorProperty: new Property( Color.grayColor( 90 ) ),
+        tandem: Tandem.OPT_OUT
+      } ) ];
+
+  const simOptions: SimOptions = {
+    credits: {
+      leadDesign: 'PhET'
+    }
+  };
+
+  const sim = new Sim( title, screens, simOptions );
+  sim.start();
+} );
