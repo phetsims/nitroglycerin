e44e7de9145ff945ffc25c79de156da8ba106419
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-20 12:16:45 -0600

    remove as many LinkableElement usages as possible, using PhetioProperty where we have to, https://github.com/phetsims/tandem/issues/299
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/NitroglycerinStrings.ts
+++ js/NitroglycerinStrings.ts
@@ -5,12 +5,12 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import LinkableProperty from '../../axon/js/LinkableProperty.js';
+import type LocalizedStringProperty from '../../chipper/js/LocalizedStringProperty.js';
 import nitroglycerin from './nitroglycerin.js';
 
 type StringsType = {
   'nitroglycerin': {
-    'titleStringProperty': LinkableProperty<string>;
+    'titleStringProperty': LocalizedStringProperty;
   }
 };
 
