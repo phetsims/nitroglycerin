03b0e470f2eca7a361851e2184740c67eaca8dbb
phet-dev <phetadmin@gmail.com>
2026-03-20 14:14:01 -0600

    grunt update  from daily grunt work

--- js/NitroglycerinStrings.ts
+++ js/NitroglycerinStrings.ts
@@ -19,4 +19,6 @@ type StringsType = {
 
 const NitroglycerinStrings = getStringModule( 'NITROGLYCERIN' ) as StringsType;
 
+nitroglycerin.register( 'NitroglycerinStrings', NitroglycerinStrings );
+
 export default NitroglycerinStrings;
